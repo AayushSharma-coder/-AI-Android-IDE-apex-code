073b7c5ff4c70cbd2f085734438784456a27aedb
AayushSharma-coder <aayushsharma2897@gmail.com>
2026-04-19 17:27:57 +0545

    Added GitHub Actions build script

---
+++ android/app/src/main/java/com/umang/apexcode/MainActivity.java
@@ -0,0 +1,5 @@
+package com.umang.apexcode;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.umang.apexcode',
+  appName: 'ApexCode',
+  webDir: 'dist'
+};
+
+export default config;
